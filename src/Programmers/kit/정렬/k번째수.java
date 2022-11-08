dddb677a7b330fa9d275537d7414bb5c675e424f
dhgustjd10 <dhgustjd10@naver.com>
2022-11-08 13:19:22 +0900

    20221108

---
+++ src/Programmers/kit/정렬/k번째수.java
@@ -0,0 +1,33 @@
+package Programmers.kit.정렬;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class k번째수 {
+
+    public int[] solution(int[] array, int[][] commands) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] arr: commands) {
+
+
+
+
+        }
+
+
+        int [] answer = {};
+
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        k번째수 k = new k번째수();
+        int [] answer = {1,5,2,6,3,7,4};
+        int [][] commands = {{2,5,3},{4,4,1},{5,6,3}};
+        k.solution(answer, commands);
+    }
+}
