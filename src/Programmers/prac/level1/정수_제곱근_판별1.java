5438a47a6fdb70fa605d39eca09430306978baa6
caprocoo <dhgustjd10@gmail.com>
2022-11-09 20:30:44 +0900

    ~정수제곱근판별

---
+++ src/Programmers/prac/level1/정수_제곱근_판별1.java
@@ -0,0 +1,27 @@
+package Programmers.prac.level1;
+
+public class 정수_제곱근_판별1 {
+    public long solution(long n) {
+
+
+        long answer = 0;
+        Double x = Math.sqrt(n);
+        if(x == x.intValue()){
+            answer = (long)Math.pow(x+1, 2);
+        }else{
+            answer = -1;
+        }
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        정수_제곱근_판별1 a = new 정수_제곱근_판별1();
+
+        long n = 123;
+        a.solution(n);
+    }
+}
