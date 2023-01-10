fb809fb09aafdd88728cb6cc90e07ada01ce14c0
caprocoo <dhgustjd10@gmail.com>
2023-01-10 17:48:11 +0900

    ~ comparable & comparator 정리

---
+++ src/theory/java/modern/chap01/ComparableExample.java
@@ -0,0 +1,53 @@
+package theory.java.modern.chap01;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * packageName    : theory.java.modern.chap01
+ * fileName       : ComparableExample
+ * author         : caprocoo
+ * date           : 2023-01-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-01-10        caprocoo       최초 생성
+ */
+public class ComparableExample {
+    public static void main(String[] args) {
+
+
+
+
+        MyInteger[] arr = new MyInteger[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = new MyInteger((int) (Math.random() * 100));
+        }
+        System.out.println("정렬 전 : ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i].value + " ");
+        }
+        System.out.println("정렬 후 : ");
+        Arrays.sort(arr);
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i].value + " ");
+        }
+
+    }
+
+    static class MyInteger implements Comparable<MyInteger> {
+        int value;
+
+        public MyInteger(int value) {
+            this.value = value;
+        }
+
+
+        @Override
+        public int compareTo(MyInteger o) {
+            return this.value - o.value;
+        }
+    }
+
+}
---
+++ src/theory/java/modern/chap01/ComparatorExample.java
@@ -0,0 +1,55 @@
+package theory.java.modern.chap01;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * packageName    : theory.java.modern.chap01
+ * fileName       : ComparatorExample
+ * author         : caprocoo
+ * date           : 2023-01-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-01-10        caprocoo       최초 생성
+ */
+public class ComparatorExample {
+
+    static Comparator<MyInteger> comp = new Comparator<MyInteger>() {
+        @Override
+        public int compare(MyInteger o1, MyInteger o2) {
+            return o2.value- o1.value;
+        }
+    };
+
+    public static void main(String[] args) {
+
+        MyInteger[] arr = new MyInteger[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = new MyInteger((int) (Math.random() * 100));
+        }
+        System.out.println("정렬 전 : ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i].value+" ");
+        }
+        System.out.println();
+
+        Arrays.sort(arr, comp);
+        System.out.println("정렬 후 : ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i].value+" ");
+        }
+
+
+    }
+
+    static class MyInteger{
+        int value;
+
+        public MyInteger(int value) {
+            this.value = value;
+        }
+
+    }
+}
