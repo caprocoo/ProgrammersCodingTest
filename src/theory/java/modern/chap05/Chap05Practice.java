e9d745ee54995d26cf72c6c761d64f3a78d1ee45
caprocoo <dhgustjd10@gmail.com>
2023-01-15 15:49:44 +0900

    chap05 완료

--- src/theory/java/modern/chap05/Chap05Practice.java
+++ src/theory/java/modern/chap05/Chap05Practice.java
@@ -24,38 +24,37 @@ import static theory.java.modern.example.Dish.menu;
  */
 public class Chap05Practice {
     public static void main(String[] args) {
-//        List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 4, 4, 1);
-//        arr.stream().filter(i -> i % 2 == 0).distinct().forEach(System.out::println);
+        List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5, 4, 4, 1);
+        arr.stream().filter(i -> i % 2 == 0).distinct().forEach(System.out::println);
 
-//        // p. 165 예제1
-//        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
-//        List<Integer> dd = integerList.stream().map(n -> n*n).collect(Collectors.toList());
-//        System.out.println(dd);
+        // p. 165 예제1
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> dd = integerList.stream().map(n -> n*n).collect(Collectors.toList());
+        System.out.println(dd);
+
+        // p.166 예제2
+        List<Integer> a = Arrays.asList(1, 2, 3);
+        List<Integer> b = Arrays.asList(3, 4);
+
+        List<int[]> list = a.stream().flatMap(i-> b.stream().map(j->new int[]{i,j})).collect(Collectors.toList());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(Arrays.toString(list.get(i)));
+        }
+
+        // p.166 예제3
+        List<int[]> list2 = a.stream().flatMap(i-> b.stream().filter(j-> (i+j)%3==0).map(j->new int[]{i,j})).collect(Collectors.toList());
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.println(Arrays.toString(list2.get(i)));
+        }
 //
-//        // p.166 예제2
-//        List<Integer> a = Arrays.asList(1, 2, 3);
-//        List<Integer> b = Arrays.asList(3, 4);
+//        Stream.iterate(0, n -> n + 2).limit(10).forEach(System.out::println);
 //
-//        List<int[]> list = a.stream().flatMap(i-> b.stream().map(j->new int[]{i,j})).collect(Collectors.toList());
-//        for (int i = 0; i < list.size(); i++) {
-//            System.out.println(Arrays.toString(list.get(i)));
-//        }
-//
-//        // p.166 예제3
-//        List<int[]> list2 = a.stream().flatMap(i-> b.stream().filter(j-> (i+j)%3==0).map(j->new int[]{i,j})).collect(Collectors.toList());
-//        for (int i = 0; i < list2.size(); i++) {
-//            System.out.println(Arrays.toString(list2.get(i)));
-//        }
-//
-//        IntStream intStream = IntStream.range(1, 100).filter(i -> i % 2 == 0);
-//        System.out.println(intStream.count());
+//        Stream.iterate(new int[]{0, 1},
+//                ints->new int[]{ints[1], ints[0]+ints[1]})
+//                .limit(20).forEach(t -> System.out.println("("+t[0] + " "+t[1]+")"));
 
 
-//        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed().flatMap(a -> IntStream.range(a, 100).filter(b -> Math.sqrt(a * a + b * b) % 1 == 0).mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
-//        pythagoreanTriples.limit(5).forEach(t -> System.out.println(t[0] + " "+t[1] + " "+t[2] ));
 
-        Stream<double[]> pythagoreanTriples2 =  IntStream.rangeClosed(1, 100).boxed().flatMap(a -> IntStream.range(a, 100).mapToObj(b -> new double[]{a, b, Math.sqrt(a * a + b * b)})).filter(t -> t[2] % 1 == 0);
-        pythagoreanTriples2.limit(5).forEach(t -> System.out.println(t[0] + " "+t[1] + " "+t[2] ));
 
     }
 
