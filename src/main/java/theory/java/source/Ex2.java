976ecd126351e203c0b6655048ef9cf8116db6a9
caprocoo <91231618+caprocoo@users.noreply.github.com>
2023-03-30 18:04:35 +0900

    ex2

--- src/main/java/theory/java/source/Ex2.java
+++ src/main/java/theory/java/source/Ex2.java
@@ -14,7 +14,19 @@ package theory.java.source;
 public class Ex2 {
     public static void main(String[] args) {
 
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        int[] evenArr = Arrays.stream(arr)
+                .filter(n -> n % 2 == 0) // 짝수인 요소만 선택
+                .toArray();
+
+        System.out.println(Arrays.toString(evenArr)); // [2, 4, 6, 8, 10]int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] evenArr = Arrays.stream(arr)
+                .filter(n -> n % 2 == 0) // 짝수인 요소만 선택
+                .toArray();
+
+        System.out.println(Arrays.toString(evenArr)); // [2, 4, 6, 8, 10]
 
 
     }
